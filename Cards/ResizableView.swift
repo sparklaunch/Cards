edbef615aa36b1981c8ed7c215a99ea692dd4446
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-08 19:08:20 +0900

    [Add] Rotation and Move.

--- Cards/ResizableView.swift
+++ Cards/ResizableView.swift
@@ -8,11 +8,20 @@
 import SwiftUI
 
 struct ResizableView: View {
+    @State private var previousRotation: Angle = .zero
     @State private var transform: Transform = .init()
     @State private var previousOffset: CGSize = .zero
     private let content = RoundedRectangle(cornerRadius: 30.0, style: .continuous)
     private let color: Color = .red
     var body: some View {
+        let rotationGesture = RotationGesture()
+            .onChanged { rotation in
+                transform.rotation += rotation - previousRotation
+                previousRotation = rotation
+            }
+            .onEnded { _ in
+                previousRotation = .zero
+            }
         let dragGesture = DragGesture()
             .onChanged { value in
                 transform.offset = value.translation + previousOffset
@@ -23,8 +32,10 @@ struct ResizableView: View {
         content
             .frame(width: transform.size.width, height: transform.size.height)
             .foregroundColor(color)
+            .rotationEffect(transform.rotation)
             .offset(transform.offset)
             .gesture(dragGesture)
+            .gesture(rotationGesture)
     }
 }
 
