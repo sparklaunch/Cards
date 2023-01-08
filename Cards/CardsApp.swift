5940a9bb1b35d44cdf0eeb62f34626fccfcdd661
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-08 13:49:56 +0900

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Jinwook Kim on 2023/01/08.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
