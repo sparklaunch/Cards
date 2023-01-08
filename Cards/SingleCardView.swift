ff5f043f6454ac3a03bdec1fb97b9a717d2298f0
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-08 14:16:48 +0900

    [Add] Navigation bar.

--- Cards/SingleCardView.swift
+++ Cards/SingleCardView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct SingleCardView: View {
+    @EnvironmentObject var viewState: ViewState
     var body: some View {
-        Color.yellow
+        NavigationView {
+            Color.yellow
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button("Done") {
+                            viewState.showAllCards.toggle()
+                        }
+                    }
+            }
+        }
     }
 }
 
 struct SingleCardView_Previews: PreviewProvider {
     static var previews: some View {
         SingleCardView()
+            .environmentObject(ViewState())
     }
 }
