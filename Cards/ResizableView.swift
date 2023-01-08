01c194dfa52bea6214184ebdc9f362be7327f2d3
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-08 18:37:42 +0900

    [Add] Transform model.

--- Cards/ResizableView.swift
+++ Cards/ResizableView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct ResizableView: View {
+    @State private var transform: Transform = .init()
     private let content = RoundedRectangle(cornerRadius: 30.0, style: .continuous)
     private let color: Color = .red
     var body: some View {
         content
-            .frame(width: 250, height: 180)
+            .frame(width: transform.size.width, height: transform.size.height)
             .foregroundColor(color)
     }
 }
---
+++ Cards/Transform.swift
@@ -0,0 +1,14 @@
+//
+//  Transform.swift
+//  Cards
+//
+//  Created by Jinwook Kim on 2023/01/08.
+//
+
+import SwiftUI
+
+struct Transform {
+    var size: CGSize = .init(width: 250, height: 180)
+    var rotation: Angle = .zero
+    var offset: CGSize = .zero
+}
