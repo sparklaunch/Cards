2e50522af6947ea0c22f469fd96f8ccb84a721da
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-15 12:15:00 +0900

    [Add] ImageElement.

---
+++ Cards/ImageElement.swift
@@ -0,0 +1,14 @@
+//
+//  ImageElement.swift
+//  Cards
+//
+//  Created by Jinwook Kim on 2023/01/15.
+//
+
+import SwiftUI
+
+struct ImageElement: CardElement {
+    let id = UUID()
+    var transform: Transform
+    var image: Image
+}
