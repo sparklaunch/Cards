a182026a953072e1df34b551d9ceab864437c917
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-15 16:23:22 +0900

    [Add] CardStore state object.

--- Cards/CardStore.swift
+++ Cards/CardStore.swift
@@ -9,4 +9,9 @@ import SwiftUI
 
 class CardStore: ObservableObject {
     @Published var cards: [Card] = []
+    init(defaultData: Bool = false) {
+        if defaultData {
+            cards = initialCards
+        }
+    }
 }
--- Cards/CardsApp.swift
+++ Cards/CardsApp.swift
@@ -10,10 +10,12 @@ import SwiftUI
 @main
 struct CardsApp: App {
     @StateObject var viewState = ViewState()
+    @StateObject var store = CardStore(defaultData: true)
     var body: some Scene {
         WindowGroup {
             CardsView()
                 .environmentObject(viewState)
+                .environmentObject(store)
         }
     }
 }
