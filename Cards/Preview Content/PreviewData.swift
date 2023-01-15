b5fc2fe59f0a1f4b674da242c89972eeb16b706d
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-15 11:59:30 +0900

    [Add] Some external files.

---
+++ Cards/Preview Content/PreviewData.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+/*
+let initialCards: [Card] = [
+  Card(backgroundColor: .green, elements: initialElements),
+  Card(backgroundColor: .orange),
+  Card(backgroundColor: .red),
+  Card(backgroundColor: .purple),
+  Card(backgroundColor: .yellow)
+]
+
+let initialElements: [CardElement] = [
+  ImageElement(
+    transform: Transform(
+      size: CGSize(width: 310, height: 225),
+      rotation: .init(degrees: 10),
+      offset: CGSize(width: 5, height: -245)),
+    image: Image("hedgehog1")),
+  ImageElement(
+    transform: Transform(
+      size: CGSize(width: 330, height: 238),
+      rotation: .init(degrees: -25),
+      offset: CGSize(width: 5, height: 200)),
+    image: Image("hedgehog2")),
+  ImageElement(
+    transform: Transform(),
+      image: Image("hedgehog3")),
+  TextElement(
+    transform: Transform(
+      size: Settings.defaultElementSize * 1.2,
+      rotation: .zero,
+      offset: CGSize(width: -35, height: -125)),
+    text: "Hedgehogs!!!",
+    textColor: .blue)
+]
+*/
---
+++ Cards/Utils/TextExtensions.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+extension Text {
+    func scalableText(font: Font = Font.system(size: 1000)) -> some View {
+        self
+            .font(font)
+            .minimumScaleFactor(0.01)
+            .lineLimit(1)
+    }
+}
