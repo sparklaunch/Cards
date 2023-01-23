948d74b08c5bd18e3674cacc82acacbdbb3dbc42
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-23 16:25:00 +0900

    [Add] Loading all stickers.

--- Cards/StickerPicker.swift
+++ Cards/StickerPicker.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct StickerPicker: View {
+    @State private var stickerNames: [String] = []
     var body: some View {
-        Group {
-            if let resourcePath = Bundle.main.resourcePath, let image = UIImage(named: resourcePath + "/Stickers/Camping/fire.png") {
-                Image(uiImage: image)
-            } else {
-                EmptyView()
+        ScrollView {
+            ForEach(stickerNames, id: \.self) { sticker in
+                Image(uiImage: image(from: sticker))
+                    .resizable()
+                    .scaledToFit()
             }
         }
+        .onAppear {
+            stickerNames = loadStickers()
+        }
+    }
+    func loadStickers() -> [String] {
+        var themes: [URL] = []
+        var stickerNames: [String] = []
+        let fileManager = FileManager.default
+        if let resourcePath = Bundle.main.resourcePath, let enumerator = fileManager.enumerator(at: URL(fileURLWithPath: resourcePath + "/Stickers"), includingPropertiesForKeys: nil, options: [.skipsSubdirectoryDescendants, .skipsHiddenFiles]) {
+            for case let url as URL in enumerator where url.hasDirectoryPath {
+                themes.append(url)
+            }
+        }
+        for theme in themes {
+            if let files = try? fileManager.contentsOfDirectory(atPath: theme.path) {
+                for file in files {
+                    stickerNames.append(theme.path + "/" + file)
+                }
+            }
+        }
+        return stickerNames
+    }
+    func image(from path: String) -> UIImage {
+        print("Loading: ", path)
+        return UIImage(named: path) ?? UIImage(named: "Error") ?? UIImage()
     }
 }
 
