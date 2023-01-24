cdd32d6d69d82fce8c605970e124a613dd57ac40
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-24 09:31:16 +0900

    [Add] PhotoPicker logics.

--- Cards/PhotoPicker.swift
+++ Cards/PhotoPicker.swift
@@ -6,20 +6,54 @@
 //
 
 import SwiftUI
+import PhotosUI
 
-struct PhotoPicker: UIViewRepresentable {
-    func makeUIView(context: Context) -> some UIView {
-        let label = UILabel()
-        label.text = "Hello UIKit!"
-        return label
+struct PhotoPicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var images: [UIImage]
+    class PhotosCoordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: PhotoPicker
+        init(parent: PhotoPicker) {
+            self.parent = parent
+        }
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            let itemProviders = results.map(\.itemProvider)
+            for itemProvider in itemProviders {
+                if itemProvider.canLoadObject(ofClass: UIImage.self) {
+                    itemProvider.loadObject(ofClass: UIImage.self) { image, error in
+                        if let error {
+                            print(error.localizedDescription)
+                        } else {
+                            DispatchQueue.main.async {
+                                if let image = image as? UIImage {
+                                    self.parent.images.append(image)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
     }
-    func updateUIView(_ uiView: UIViewType, context: Context) {
+    func makeUIViewController(context: Context) -> some UIViewController {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        configuration.selectionLimit = 0
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = context.coordinator
+        return picker
+    }
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
         // NOTHING TO DO…
+    }
+    func makeCoordinator() -> PhotosCoordinator {
+        PhotosCoordinator(parent: self)
     }
 }
 
 struct PhotoPicker_Previews: PreviewProvider {
     static var previews: some View {
-        PhotoPicker()
+        PhotoPicker(images: .constant([UIImage]()))
     }
 }
